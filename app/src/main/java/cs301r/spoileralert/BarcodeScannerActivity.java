ddd034d1d8118ec0662d915a7932929440630447
AndersenJ <johnathan.andersen@byu.edu>
2017-10-19 00:32:59 -0600

    UPC lookup now works

--- app/src/main/java/cs301r/spoileralert/BarcodeScannerActivity.java
+++ app/src/main/java/cs301r/spoileralert/BarcodeScannerActivity.java
@@ -2,26 +2,27 @@ package cs301r.spoileralert;
 
 import android.content.Context;
 import android.content.Intent;
-import android.preference.PreferenceActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.google.zxing.Result;
 import com.loopj.android.http.JsonHttpResponseHandler;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
-import me.dm7.barcodescanner.zxing.ZXingScannerView;
+import cz.msebera.android.httpclient.Header;
+import me.dm7.barcodescanner.zbar.*;
+//import me.dm7.barcodescanner.zxing.ZXingScannerView;
 
 
-public class BarcodeScannerActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler {
-    private ZXingScannerView mScannerView;
+public class BarcodeScannerActivity extends AppCompatActivity implements ZBarScannerView.ResultHandler {
+    private ZBarScannerView mScannerView;
 
     @Override
     public void onCreate(Bundle state) {
         super.onCreate(state);
-        mScannerView = new ZXingScannerView(this);   // Programmatically initialize the scanner view
+        mScannerView = new ZBarScannerView(this);   // Programmatically initialize the scanner view
         setContentView(mScannerView);                // Set the scanner view as the content view
     }
 
@@ -44,22 +45,29 @@ public class BarcodeScannerActivity extends AppCompatActivity implements ZXingSc
         //Log.v(TAG, rawResult.getText()); // Prints scan results
         //Log.v(TAG, rawResult.getBarcodeFormat().toString()); // Prints the scan format (qrcode, pdf417 etc.)
 
-        Context context = mScannerView.getContext();
-        UPCRequest.get(rawResult.getText(), null, new JsonHttpResponseHandler() {
+        UPCRequest.get(rawResult.getContents(), null, new JsonHttpResponseHandler() {
 
-            //@Override
-            public void onSuccess(int statusCode, PreferenceActivity.Header[] headers, JSONArray response) {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
                 // Pull out the first event on the public timeline
                 //JSONObject firstEvent = timeline.get(0);
                 //String tweetText = firstEvent.getString("text");
+                try {
+                    Context context = mScannerView.getContext();
+                    Intent i = new Intent(context, AddFoodActivity.class);
+                    JSONObject items = response.getJSONArray("items").getJSONObject(0);
+                    String title = items.getString("title");
+                    i.putExtra("FOOD_NAME", title);
+                    context.startActivity(i);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
 
                 // Do something with the response
                 //System.out.println(tweetText);
             }
         });
-        Intent i = new Intent(context, AddFoodActivity.class);
-        i.putExtra("FOOD_NAME", rawResult.getText());
-        context.startActivity(i);
+
         // If you would like to resume scanning, call this method below:
         //mScannerView.resumeCameraPreview(this);
     }
